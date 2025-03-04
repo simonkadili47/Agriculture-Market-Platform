497ac6545a315299473db4fee1e77c4e1aefdc3d
simonkadili47 <simonkadili47@gmail.com>
2025-03-04 22:49:08 +0300

    fix_login

--- src/Login.jsx
+++ src/Login.jsx
@@ -62,20 +62,20 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white">
+    <div className="flex items-center justify-center min-h-screen bg-blue-50">
       <ToastContainer />
-      <div className="w-full max-w-sm p-8 bg-white rounded-lg shadow-md">
-        <h1 className="mb-6 text-2xl font-bold text-center text-gray-700">Login</h1>
+      <div className="w-full max-w-sm p-8 bg-white rounded-lg">
+        <h1 className="mb-6 text-2xl font-bold text-center text-black">Login</h1>
         {error && <p className="mb-4 text-sm text-red-600">{error}</p>}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-600">
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">
               Email
             </label>
             <input
               type="email"
               id="email"
-              className="w-full px-3 py-2 text-sm text-gray-700 border rounded-lg focus:outline-none focus:border-indigo-500"
+              className="w-full px-3 py-2 text-sm text-neutral-500 border rounded-lg focus:outline-none focus:border-black"
               placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -83,13 +83,13 @@ const Login = () => {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-600">
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-black">
               Password
             </label>
             <input
               type="password"
               id="password"
-              className="w-full px-3 py-2 text-sm text-gray-700 border rounded-lg focus:outline-none focus:border-indigo-500"
+              className="w-full px-3 py-2 text-sm text-neutral-700 border rounded-lg focus:outline-none focus:border-black"
               placeholder="Enter your password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -99,7 +99,7 @@ const Login = () => {
           <div className="flex items-center justify-between mb-4">
             <div className="flex items-center">
               <input type="checkbox" id="rememberMe" className="w-4 h-4 text-indigo-600 border-gray-300 rounded" />
-              <label htmlFor="rememberMe" className="ml-2 text-sm text-gray-600">
+              <label htmlFor="rememberMe" className="ml-2 text-sm text-black">
                 Remember me
               </label>
             </div>
@@ -107,12 +107,12 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 text-sm font-bold text-white bg-indigo-600 rounded-lg hover:bg-indigo-700"
+            className="w-full px-4 py-2 text-sm font-bold text-white bg-indigo-600 rounded-lg"
           >
             Login
           </button>
           <div className="mt-4 text-center">
-            <span className="text-sm text-gray-600">
+            <span className="text-sm text-neutral-600">
               New Here? <Link to="/register" className="text-indigo-600 hover:underline pl-2">Create Account</Link>
             </span>
           </div>
